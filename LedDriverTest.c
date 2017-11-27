707cfa384656a4e6ef708596cb6a6ba055a22dae
kristofersomlin <kristofer.somlin@gmail.com>
2017-11-27 13:34:04 +0100

    added more led commands

--- LedDriverTest.c
+++ LedDriverTest.c
@@ -1,15 +1,44 @@
 #include "unity/src/unity.h"
-void TEST_LEDS() {
+uint16_t virtual_bit = 0xffff;
 
+
+void TEST_LEDS() {
+  TEST_ASSERT_EQUAL(0, virtual_bit);
+}
+
+void INIT_LEDS() {
+  virtual_bit = 0x0000;
+}
+
+void TURN_OFF_LED(int LedNR) {
+  if(LedNR >= 0 && LedNR < 16) {
+    virtual_bit &= ~(1<<LedNR);
+  }
+}
+void TURN_ON_LED(int LedNR) {
+  if(LedNR >= 0 && LedNR < 16) {
+    virtual_bit |= (1<<LedNR);
+  }
+}
+
+void PRINT_LED_VALUES(uint16_t num) {
+  int bit=0;
+   for(;bit<(sizeof(unsigned int) * 8); bit++)
+   {
+      printf("%i ", num & 0x01);
+      num = num >> 1;
+   }
+   printf("\n");
 }
 
 int main(void) {
+
+  INIT_LEDS();
+
+
+
+
   UNITY_BEGIN();
   RUN_TEST(TEST_LEDS);
-
   return UNITY_END();
 }
-
-
-
-ok
--- makefile
+++ makefile
@@ -5,4 +5,4 @@ UNITY_PATH_AND_FILE=unity/src/unity.c
 all:
 	${C_COMPILER} ${MAIN_FILE} -o main.exe
 test:
-	${C_COMPILER} ${MAIN_FILE} ${TEST_FILE} ${FILE_TO_TEST} ${UNITY_PATH_AND_FILE} -I \unity\extras\fixture\src -o test.exe
+	${C_COMPILER} ${FILE_TO_TEST} ${UNITY_PATH_AND_FILE} -o test.exe

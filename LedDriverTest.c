9a1e3222b06ade24e7c6249c522e9510fcf5c3a1
kristofersomlin <kristofer.somlin@gmail.com>
2017-11-27 14:24:17 +0100

    1

--- LedDriverTest.c
+++ LedDriverTest.c
@@ -1,44 +1,77 @@
 #include "unity/src/unity.h"
 uint16_t virtual_bit = 0xffff;
-
-
-void TEST_LEDS() {
-  TEST_ASSERT_EQUAL(0, virtual_bit);
-}
-
-void INIT_LEDS() {
-  virtual_bit = 0x0000;
-}
-
-void TURN_OFF_LED(int LedNR) {
-  if(LedNR >= 0 && LedNR < 16) {
+void TURN_OFF_LED(int LedNR)
+{
+  if(LedNR >= 0 && LedNR < 16)
+  {
     virtual_bit &= ~(1<<LedNR);
   }
 }
-void TURN_ON_LED(int LedNR) {
-  if(LedNR >= 0 && LedNR < 16) {
+
+void TURN_ON_LED(int LedNR)
+{
+  if(LedNR >= 0 && LedNR < 16)
+  {
     virtual_bit |= (1<<LedNR);
   }
 }
 
-void PRINT_LED_VALUES(uint16_t num) {
-  int bit=0;
-   for(;bit<(sizeof(unsigned int) * 8); bit++)
-   {
-      printf("%i ", num & 0x01);
-      num = num >> 1;
-   }
-   printf("\n");
+void CHANGE_ALL_LEDS(int on)
+{
+  int i=0;
+  for (;i<16;i++)
+  {
+    if(on)
+    {
+      TURN_ON_LED(i);
+    }
+    else
+    {
+      TURN_OFF_LED(i);
+    }
+  }
 }
 
-int main(void) {
+void PRINT_LED_VALUES(uint16_t num)
+{
+  int bit=0;
+  for(;bit<(sizeof(unsigned int) * 8); bit++)
+  {
+    printf("%i ", num & 0x01);
+    num = num >> 1;
+  }
+  printf("\n");
+}
 
-  INIT_LEDS();
 
 
 
 
+
+
+void TEST_ALL_LEDS()
+{
+  CHANGE_ALL_LEDS(0);
+  TEST_ASSERT_EQUAL(0, virtual_bit);
+  CHANGE_ALL_LEDS(1);
+  TEST_ASSERT_EQUAL(65535, virtual_bit);
+}
+
+void TEST_ONE_LED()
+{
+  CHANGE_ALL_LEDS(0);
+  TURN_ON_LED(0);   // 0000 0000 0000 0001
+  TEST_ASSERT_EQUAL(1, virtual_bit);
+  TURN_ON_LED(2);  // 0000 0000 0000 0101 since the first bit is already set
+  TEST_ASSERT_EQUAL(5, virtual_bit);
+  TURN_OFF_LED(0);
+  TEST_ASSERT_EQUAL(4, virtual_bit);
+}
+
+int main(void)
+{
   UNITY_BEGIN();
-  RUN_TEST(TEST_LEDS);
+  RUN_TEST(TEST_ALL_LEDS);
+  RUN_TEST(TEST_ONE_LED);
   return UNITY_END();
 }

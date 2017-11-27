bdafd07befe9a9afd38d00b4c1e20eb2c4d3c64a
kristofersomlin <kristofer.somlin@gmail.com>
2017-11-27 11:18:12 +0100

    new files

---
+++ LedDriverTest.c
@@ -0,0 +1,16 @@
+#include "unity\src\unity.c"
+#include "unity\extras\fixture\src\unity_fixture.h"
+TEST_GROUP(LedDriver);
+
+TEST_SETUP(LedDriver)
+{
+
+}
+TEST_TEAR_DOWN(LedDriver)
+{
+
+}
+TEST(LedDriver, LedsOffAfterCreate)
+{
+  TEST_FAIL_MESSAGE("Start here");
+}
---
+++ makefile
@@ -0,0 +1,8 @@
+C_COMPILER=gcc
+MAIN_FILE=main.c
+FILE_TO_TEST=LedDriverTest.c
+UNITY_PATH_AND_FILE=unity/src/unity.c
+all:
+	${C_COMPILER} ${MAIN_FILE} -o main.exe
+test:
+	${C_COMPILER} ${MAIN_FILE} ${TEST_FILE} ${FILE_TO_TEST} ${UNITY_PATH_AND_FILE} -o test.exe
